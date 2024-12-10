cbbdfe145aeceb1b5b3676349f53797a0d5c5e73
Jan Zimmermann <jaenu.zimmermann@gmail.com>
2024-12-10 12:15:56 +0100

    Add FX package; Finished AB08

--- Assets/Scripts/Gravitation.cs
+++ Assets/Scripts/Gravitation.cs
@@ -5,16 +5,21 @@ public class Gravitation : MonoBehaviour
 {
     public Transform center;
     public float massEarth = 5.972e24f;
-    public float massComet = 1e11f;
+    public float massComet = 1e8f;
     
-    public float radiusEarth = 6371f;
+    public float radiusEarth = 6.371f;
     public Vector3 initialVelocity;
     
     public float G = 6.67430e-11f;
 
+    public bool showFlightCurve = false;
+    
+    public GameObject Explosion;
+    
+    // v1 = -2,0,1.5
+    // v2 = -2,0,1
     private Vector3 _v;
     private float _scalingFactor = 1e6f;
-    private bool isStarted = false;
     private float _dt;
 
     void Start()
@@ -22,18 +27,10 @@ public class Gravitation : MonoBehaviour
         _v = initialVelocity;
         _dt = Time.fixedDeltaTime;
     }
-
-    void Update()
-    {
-        if (Input.GetKey("space"))
-        {
-            isStarted = true;
-        }
-    }
+    
 
     void FixedUpdate()
     {
-        if (!isStarted) return;
 
         // distance between earth and comet (scaled)
         Vector3 direction = (center.position - transform.position) * _scalingFactor;
@@ -50,7 +47,12 @@ public class Gravitation : MonoBehaviour
         _v += acceleration * _dt;
         transform.position += _v * _dt;
         
-        if (distance < (radiusEarth / _scalingFactor))
+        if (showFlightCurve)
+        {
+            SpawnFlightCurvePoint();
+        }
+        Debug.Log(distance + " < " + radiusEarth * _scalingFactor);
+        if (distance < radiusEarth * _scalingFactor)
         {
             BurnUp();
         }
@@ -58,7 +60,18 @@ public class Gravitation : MonoBehaviour
 
     private void BurnUp()
     {
-        Debug.Log("Komet ist in der Erdatmosphäre verglüht!");
-        Destroy(gameObject);
+        GameObject explosion = Instantiate(Explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject, 0.1f);
+        Destroy(explosion, 4f);
+    }
+    
+    private void SpawnFlightCurvePoint()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = transform.position;
+        sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f); // Kleinere Kugel
+        sphere.GetComponent<Renderer>().material.color = Color.green;
+
+        Destroy(sphere, 10f);
     }
 }
